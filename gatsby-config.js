c7f6a5cbf870e49c59ec4ed7c104e18286f44340
philippzach <mail@philippzach.com>
2020-04-03 14:25:04 -0600

    added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,46 +45,73 @@ module.exports = {
         icon: `static/favicon.png`, // This path is relative to the root of the site.
         icons: [
           {
-            "src": "favicons/icon-48x48.png",
-            "sizes": "48x48",
-            "type": "image/png"
+            src: "favicons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
           },
           {
-            "src": "favicons/icon-72x72.png",
-            "sizes": "72x72",
-            "type": "image/png"
+            src: "favicons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
           },
           {
-            "src": "favicons/icon-96x96.png",
-            "sizes": "96x96",
-            "type": "image/png"
+            src: "favicons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
           },
           {
-            "src": "favicons/icon-144x144.png",
-            "sizes": "144x144",
-            "type": "image/png"
+            src: "favicons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
           },
           {
-            "src": "favicons/icon-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
+            src: "favicons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            "src": "favicons/icon-256x256.png",
-            "sizes": "256x256",
-            "type": "image/png"
+            src: "favicons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
           },
           {
-            "src": "favicons/icon-384x384.png",
-            "sizes": "384x384",
-            "type": "image/png"
+            src: "favicons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
           },
           {
-            "src": "favicons/icon-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
-          }
+            src: "favicons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
         ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-162750314-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        //anonymize: true,
+        // Setting this parameter is also optional
+        //respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        //exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        //optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        //experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        //variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Any additional optional fields
+        //sampleRate: 5,
+        //siteSpeedSampleRate: 10,
+        //cookieDomain: "example.com",
       },
     },
     {
